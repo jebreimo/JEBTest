14d97c60a7e3701ad8c5fdba2a593701fdccad6b
Jan Erik Breimo <jebreimo@gmail.com>
2013-04-28 01:25:58 +0200

    Moved JEBTest files to JEB/Test folder.
    Makes it possible to have the JEBTest.hpp file in the JEB folder

---
+++ JEB/JEBTest.hpp
@@ -0,0 +1,9 @@
+#ifndef JEB_JEBTEST_HPP
+#define JEB_JEBTEST_HPP
+
+#include "Test/AutoSuite.hpp"
+#include "Test/AutoSuiteRunner.hpp"
+#include "Test/Macros.hpp"
+#include "Test/Session.hpp"
+
+#endif
--- JEBTest/JEBTest.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef JEB_JEBUNIT_HPP
-#define JEB_JEBUNIT_HPP
-
-#include "AutoSuite.hpp"
-#include "AutoSuiteRunner.hpp"
-#include "Macros.hpp"
-#include "Session.hpp"
-
-#endif
